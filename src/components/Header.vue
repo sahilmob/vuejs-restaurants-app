cdfed868e68837fe02badd196c3f1c2fcbd5cdf7
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-12 22:06:36 +0300

    Add html structure for Header and Footer components

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="row">
+        <nav class="navbar navbar-toggleable-md navbar-light">
+            <a href="/" class="navbar-brand">Pizza Planet</a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">Menu</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
