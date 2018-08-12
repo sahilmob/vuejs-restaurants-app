4233155c94c75220320494a4374a68539d562c5a
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-12 22:14:33 +0300

    Add html structure for Home component and import Header, Footer, and home components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <div id="app">
+    <div class="container">
+      <pp-header> </pp-header>
+      <pp-home></pp-home>
+      <pp-footer></pp-footer>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+
+export default {
+  components: {
+    ppHeader: Header,
+    ppFooter: Footer,
+    ppHome: Home
+  }
+};
 </script>
 
 <style>
---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="row">
+        <div id="home" class="col-sm-12 text-center">
+            <div class="background">
+                <h1>Welcome to Pizza Planet</h1>
+                <h2>Feeling Hungry?</h2>
+                <button class="btn btn-success">Let's order!</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
