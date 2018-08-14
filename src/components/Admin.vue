804c2f7c33dee2adf4e666071657b31ddd18eb71
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-14 05:49:55 +0300

    Create login component and add it to admin component

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -57,14 +57,22 @@
                 </table>
             </div>
         </div>
+        <hr>
+        <div class="row">
+            <div class="col-sm-12 col-lg-6">
+                <pp-login></pp-login>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import NewPizza from "./NewPizza";
+import Login from "./Login";
 export default {
   components: {
-    ppNewPizza: NewPizza
+    ppNewPizza: NewPizza,
+    ppLogin: Login
   }
 };
 </script>
