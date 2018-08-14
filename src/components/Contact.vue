4c96ccdcd7668339d3a556a3ac64118bf8e48349
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-14 06:13:56 +0300

    Add About, Contact, Delivery, History, and Ordering guide componenets

---
+++ src/components/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card">
+        <div class="card-block">
+            <h4 class="card-title">Contact</h4>
+            <p class="card-text">
+                <address>
+                    <strong>
+                        Pizza Planet
+                    </strong>
+                    <br> Address line 1<br> Address line 2<br>Phone (123) 456-7890
+                </address>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/Delivery.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card">
+        <div class="card-block">
+            <h4 class="card-title">Delivery info</h4>
+            <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui impedit, suscipit ducimus quia rem id assumenda iste iure mollitia harum doloribus voluptas eos illo maxime in excepturi. Temporibus, quaerat consectetur.
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
