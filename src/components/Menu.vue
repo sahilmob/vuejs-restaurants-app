1512167dabf8befd0788182bb978d02104574089
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-15 06:14:41 +0300

    Move dummy menu data from Menu.vue to the store

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -16,8 +16,8 @@
             </td>
           </tr>
           <tr v-for="option in item.options">
-            <td>option.sizd</td>
-            <td>option.price</td>
+            <td>{{option.size}}</td>
+            <td>{{option.price}}</td>
             <td>
               <button class="btn btn-sm btn-outline-success" type="button" @click="addToBasket(item, option)">+</button>
             </td>
@@ -62,55 +62,14 @@
 export default {
   data() {
     return {
-      menuItems: {
-        1: {
-          name: "Margherita",
-          description: "A delicious tomato based pizza topped with mozzarella",
-          options: [
-            {
-              size: 9,
-              price: 6.95
-            },
-            {
-              size: 12,
-              price: 10.95
-            }
-          ]
-        },
-        2: {
-          name: "Pepperoni",
-          description:
-            "A delicious tomato based pizza topped with mozzarella and pepperoni",
-          options: [
-            {
-              size: 9,
-              price: 7.95
-            },
-            {
-              size: 12,
-              price: 12.95
-            }
-          ]
-        },
-        3: {
-          name: "Ham and Pineapple",
-          description:
-            "A delicious tomato based pizza topped with mozzarella, ham and pineapple",
-          options: [
-            {
-              size: 9,
-              price: 7.95
-            },
-            {
-              size: 12,
-              price: 12.95
-            }
-          ]
-        }
-      },
       basket: [],
       basketText: "Your basket is empty!"
     };
+  },
+  computed: {
+    menuItems() {
+      return this.$store.state.menuItems;
+    }
   },
   methods: {
     addToBasket(item, option) {
