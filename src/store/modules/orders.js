89e410bfeda1d7e3aa5817884b2acd0fe5f6cf07
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-15 23:50:37 +0300

    Move the logic for menu, orders, and users to a sepreate module file for each

---
+++ src/store/modules/orders.js
@@ -0,0 +1,23 @@
+const state = {
+    orders: []
+}
+
+const getters = {
+    numberOfOrders: state => state.orders.length
+}
+
+const mutations = {
+    addOrder: (state, orders) => { state.orders.push(orders) }
+}
+
+
+const actions = {
+
+}
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions
+}
