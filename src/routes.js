94a161d8992fea2ed893711e75d4b27528951386
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-17 01:59:16 +0300

    Add lazy loading to menu, about, and admin component

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 import Home from './components/Home.vue'
-import Menu from './components/Menu.vue'
-import Admin from './components/Admin.vue'
-import About from './components/About.vue'
+// Lazy loading Menu section
+const Menu = () => import('./components/Menu.vue')
+// Lazy loading admin section
+const Admin = () => import('./components/Admin.vue')
+// Lazy loading about section
+const About = () => import('./components/About.vue')
 import Contact from './components/Contact.vue'
 import History from './components/History.vue'
 import Delivery from './components/Delivery.vue'
